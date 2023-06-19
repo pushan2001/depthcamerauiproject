b061ca7ebd13e8f7ea2e342f4564ef1877a1719e
pushan2001 <khondoker.alam@mail.utoronto.ca>
2023-06-19 01:44:29 -0400

    editing the tutorial.cpp file and added a helper file

--- tutorial/tutorial.cpp
+++ tutorial/tutorial.cpp
@@ -1,1 +1,10 @@
+#include <opencv2/opencv.hpp>
+#include <librealsense2/rs.hpp>
+/*This is a tutorial to get an output from the camera*/
+
+#include "cv-helper.hpp"
+
+//Setting the dimensions of the window
+const size_t windowwidth = 480;
+const size_t windowheight = 270;
 
